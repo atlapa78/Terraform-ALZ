130597cf0366a34641df25405f147fd9551df9dd
Alberto Tlapa-ADM <alberto.tlapa.adm@convergetp.com>
2023-08-02 22:47:55 +0000

    Added 2 files to /loganalyticsWS

---
+++ loganalyticsWS/variables.tf
@@ -0,0 +1,32 @@
+variable "rgname" {
+  type        = string
+  description = "resource group for laws"
+}
+
+
+variable "location" {
+  type        = string
+  description = "location of the resources"
+}
+
+variable "laws_name" {
+  type        = string
+  description = "name of resource group"
+}
+
+variable "laws_sku" {
+  type        = string
+  description = "SKU for the log analytics workspace"
+  validation {
+    condition     = contains(["Free", "Standard", "CapacityReservation", "PerGB2018"], var.laws_sku)
+    error_message = "Valid values for the log analytics work space variable are Free, Standard, CapacityReservation"
+  }
+  default = "PerGB2018"
+}
+
+variable "retention_days" {
+  type        = number
+  description = "retention in days for log analytics work space"
+  default     = 30
+}
+
