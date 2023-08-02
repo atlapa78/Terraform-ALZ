aa25e2b60aba7014eb781787695889c9fbccc4da
Alberto Tlapa-ADM <alberto.tlapa.adm@convergetp.com>
2023-08-02 22:57:27 +0000

    Added 2 files to /vm

---
+++ vm/variables.tf
@@ -0,0 +1,64 @@
+variable "location" {
+  type        = string
+  description = "location of resources"
+}
+
+variable "rgname" {
+  type        = string
+  description = "Nameg of the resource group"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "ID of the subnet to allocate the vm nic"
+
+}
+
+variable "keyvault_id" {
+  type        = string
+  description = "ID of the keyvault used for the genrated password"
+}
+
+variable "vm_name" {
+  type        = string
+  description = "virtual machine name"
+}
+
+variable "vm_size" {
+  type        = string
+  description = "virtual machine size"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "username"
+  default     = "azureuser"
+}
+
+variable "vm_publisher" {
+  type        = string
+  description = "Virtual machine publisher"
+}
+
+variable "vm_offer" {
+  type        = string
+  description = "Virtual machine offer"
+}
+
+variable "vm_sku" {
+  type        = string
+  description = "Virtual machine sku"
+}
+
+variable "vm_version" {
+  type        = string
+  description = "Virtual machine os version"
+  default     = "latest"
+}
+
+
+variable "keyvault_secret_name" {
+  type        = string
+  description = "virtual machine secret name"
+  default     = "vmpassword"
+}
