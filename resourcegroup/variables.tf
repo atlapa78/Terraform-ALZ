42f103c57cc29f366c57f93fc50d4e094cfc7189
Alberto Tlapa-ADM <alberto.tlapa.adm@convergetp.com>
2023-08-02 22:54:16 +0000

    Added 2 files to /resourcegroup

---
+++ resourcegroup/variables.tf
@@ -0,0 +1,9 @@
+variable "rgname" {
+  type        = string
+  description = "name of resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "location of the resources"
+}
