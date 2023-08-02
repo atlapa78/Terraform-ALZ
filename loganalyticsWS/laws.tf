487a6f729d5dd299d404bc76025d3a7893847dcd
Alberto Tlapa-ADM <alberto.tlapa.adm@convergetp.com>
2023-08-02 22:47:31 +0000

    Added laws.tf

---
+++ loganalyticsWS/laws.tf
@@ -0,0 +1,7 @@
+resource "azurerm_log_analytics_workspace" "laws" {
+  name                = var.laws_name
+  location            = var.location
+  resource_group_name = var.rgname
+  sku                 = var.laws_sku
+  retention_in_days   = var.retention_days
+}
