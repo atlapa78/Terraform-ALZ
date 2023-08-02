4273cf5afde938ce8c98dc665669df56b85f5407
Alberto Tlapa-ADM <alberto.tlapa.adm@convergetp.com>
2023-08-02 22:55:19 +0000

    Added sa.tf

---
+++ storageaccount/sa.tf
@@ -0,0 +1,11 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.storageaccountname
+  resource_group_name      = var.rgname
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  account_kind             = var.account_kind
+  tags = {
+    environment = "Testing"
+  }
+}
