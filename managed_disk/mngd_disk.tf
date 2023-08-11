65d2c1bb0394859cc50ec09926a498f6040534fa
Alberto Tlapa <alberto.tlapa@exactlyit.com>
2023-08-11 16:09:46 -0600

    adding disk module

---
+++ managed_disk/mngd_disk.tf
@@ -0,0 +1,8 @@
+resource "azurerm_managed_disk" "disk" {
+  name                 = "${local.vm_name}-disk1"
+  location             = azurerm_resource_group.example.location
+  resource_group_name  = azurerm_resource_group.example.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
