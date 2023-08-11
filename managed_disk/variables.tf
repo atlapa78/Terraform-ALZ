0c77db85f16c40690af833d46af6bd16bd1bba95
Alberto Tlapa <alberto.tlapa@exactlyit.com>
2023-08-11 16:15:06 -0600

    adding variables of the disk module

---
+++ managed_disk/variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+  type        = string
+  description = "location of the resources"
+}
+
+variable "rgname" {
+  type        = string
+  description = "resource group for key vault"
+
+}
+
+variable "disk_name" {
+  type        = string
+  description = "name of the managed disk"
+  
+}
