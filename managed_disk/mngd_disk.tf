740e5bc80873494125ad5da129a3294de5ace62f
Alberto Tlapa <alberto.tlapa@exactlyit.com>
2023-08-11 16:14:40 -0600

    adding managed disk module

--- managed_disk/mngd_disk.tf
+++ managed_disk/mngd_disk.tf
@@ -1,5 +1,5 @@
 resource "azurerm_managed_disk" "disk" {
-  name                 = "${local.vm_name}-disk1"
+  name                 = var.disk_name
   location             = azurerm_resource_group.example.location
   resource_group_name  = azurerm_resource_group.example.name
   storage_account_type = "Standard_LRS"
