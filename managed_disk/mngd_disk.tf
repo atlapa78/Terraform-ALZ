4e176b3c520fd5b44bedc5b74973cb459268d5e2
Alberto Tlapa <alberto.tlapa@exactlyit.com>
2023-08-11 16:35:43 -0600

    updating disks module

--- managed_disk/mngd_disk.tf
+++ managed_disk/mngd_disk.tf
@@ -1,8 +1,8 @@
 resource "azurerm_managed_disk" "disk" {
   name                 = var.disk_name
-  location             = azurerm_resource_group.example.location
-  resource_group_name  = azurerm_resource_group.example.name
-  storage_account_type = "Standard_LRS"
+  location             = var.location
+  resource_group_name  = var.rgname
+  storage_account_type = var.disk_type
   create_option        = "Empty"
-  disk_size_gb         = 10
+  disk_size_gb         = var.disk_size
 }
