d2cca7f1a858c6a5196e43b484564dae25e1b8dd
Alberto Tlapa-ADM <alberto.tlapa.adm@convergetp.com>
2023-12-05 21:46:15 +0000

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -14,10 +14,10 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "5f94af94-2e21-425d-a715-e2b7df92f974"
-  client_id       = "bac0f6eb-ced4-4143-969e-5f6f46ff85f3"
-  client_secret   = "M7q8Q~ztLAjtQlFlC9948PbzWWp3IDC5gRegWbmI"
-  tenant_id       = "4ae54b05-b77e-4224-aef1-8661422e0816"
-}
+provider "azurerm" { }
+//   features {}
+//   subscription_id = "5f94af94-2e21-425d-a715-e2b7df92f974"
+//   client_id       = "bac0f6eb-ced4-4143-969e-5f6f46ff85f3"
+//   client_secret   = "M7q8Q~ztLAjtQlFlC9948PbzWWp3IDC5gRegWbmI"
+//   tenant_id       = "4ae54b05-b77e-4224-aef1-8661422e0816"
+// }
