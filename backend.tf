7dfe7a88bc8b911c562f623799be101fa770e184
Alberto Tlapa-ADM <alberto.tlapa.adm@convergetp.com>
2023-12-06 17:16:38 +0000

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -11,7 +11,7 @@ terraform {
     storage_account_name = "alcavdes1tsa1"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
-
+  }
   //   subscription_id = "5f94af94-2e21-425d-a715-e2b7df92f974"
   //   //client_id       = "03ac8667-9625-46c6-becf-bf206d0cd680"
   //   client_id = "bac0f6eb-ced4-4143-969e-5f6f46ff85f3"
