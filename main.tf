df10355e223f79aa8ed951974bf3cb0fd0f09e71
Alberto Tlapa-ADM <alberto.tlapa.adm@convergetp.com>
2023-12-06 19:28:30 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ terraform {
     }
 
   }
+  
+  backend "azurerm" {
+
+    resource_group_name  = "Terraform-lab-rg"
+    storage_account_name = "alcavdes1tsa1"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
