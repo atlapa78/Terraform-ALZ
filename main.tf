43809cc6880479799d53d661e265690dea5b358a
Alberto Tlapa-ADM <alberto.tlapa.adm@convergetp.com>
2023-12-06 16:47:01 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.54.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+
+  }
+}
+
+
 data "azurerm_client_config" "current" {}
 
 locals {
